da691a731860e036c83f171e59eff73a37f1927c
Adi Iyengar <aditya7iyengar@gmail.com>
2017-08-09 13:05:00 -0400

    Start work. WIP:
    - Implement behavior functions

--- lib/ecto_csv.ex
+++ lib/ecto_csv.ex
@@ -1,2 +1,10 @@
 defmodule EctoCSV do
+  @behaviour Ecto.Adapter
+  @behaviour Ecto.Adapter.Storage
+  @behaviour Ecto.Adapter.Migration
+
+  @doc false
+  defmacro __before_compile__(env) do
+
+  end
 end
