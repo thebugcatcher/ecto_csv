5b338f09989b55568718a770bc0eb3c9389ea25e
Adi Iyengar <aditya7iyengar@gmail.com>
2017-08-09 15:22:25 -0400

    This will be api through which csv parser can be accessed

---
+++ lib/ecto_csv/table.ex
@@ -0,0 +1,14 @@
+defmodule EctoCSV.Table do
+
+  @doc """
+  Fetches all the data assocaited with a schema.
+  It takes in a `schema` and a `loader`
+  """
+  def fetch_data(schema, loader)
+  def fetch_data(schema, :eager) do
+
+  end
+  def fetch_data(schema, :lazy) do
+
+  end
+end
